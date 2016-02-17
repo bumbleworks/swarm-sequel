2cbad411f614b2d35490d1a162378c24596fb41a
Ravi Gadad <ravi@renewfund.com>
2016-02-16 23:45:51 -0800

    Initial commit - bundle gem skeleton

---
+++ lib/swarm/sequel.rb
@@ -0,0 +1,7 @@
+require "swarm/sequel/version"
+
+module Swarm
+  module Sequel
+    # Your code goes here...
+  end
+end
---
+++ lib/swarm/sequel/version.rb
@@ -0,0 +1,5 @@
+module Swarm
+  module Sequel
+    VERSION = "0.1.0"
+  end
+end
